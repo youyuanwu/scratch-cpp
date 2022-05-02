fa2b2719b432014b95648b79e96387dd1fabce59
Youyuan Wu <youyuanwu@outlook.com>
2022-05-01 19:44:46 -0700

    asio windows bind fail

--- asio/asio1.cpp
+++ asio/asio1.cpp
@@ -1,3 +1,27 @@
+#include "asio.hpp"
+#include <string>
+#include <iostream>
+
 int main(){
-  
+  try
+    {
+    asio::io_context ioc{1};
+
+    std::string name("server.sock");
+        // bool d = DeleteFileA(name.c_str());
+        // if(!d){
+        //     DWORD err = GetLastError();
+        //     if(err != ERROR_FILE_NOT_FOUND){
+        //         std::cerr << "LastError: " << err << std::endl;
+        //         return EXIT_FAILURE;
+        //     }
+        // }
+    asio::local::stream_protocol::endpoint ep(name);
+    asio::local::stream_protocol::acceptor acceptor(ioc, ep);
+    }
+    catch(std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
