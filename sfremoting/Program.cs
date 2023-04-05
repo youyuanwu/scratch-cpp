9f7df28abf9208a28ffef0ae8cce6c3ee8a5a67b
Youyuan Wu <youyuanwu@microsoft.com>
2023-04-05 10:43:06 -0700

    add sfremoting example

---
+++ sfremoting/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+public interface IMyService : IService
+{
+    Task<string> HelloWorldAsync();
+}
+
+class MyService : StatelessService, IMyService
+{
+    public MyService(System.Fabric.StatelessServiceContext context)
+        : base (context)
+    {
+    }
+
+    public Task<string> HelloWorldAsync()
+    {
+        return Task.FromResult("Hello!");
+    }
+
+    protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+    {
+     return this.CreateServiceRemotingInstanceListeners();
+    }
+}
+
+class Program
+{
+  public static void Main() { 
+    // See https://aka.ms/new-console-template for more information
+    Console.WriteLine("Hello, World!");
+  }
+}
